2b52a3660c5456d45d2551016d1f100189e6867e
tetron225 <skim6502@gmail.com>
2025-02-16 21:57:00 -0500

    readjusted logic for each boxcheck. works perfectly. need to start using DOM manipulation to put it up visually

--- tictactoe.js
+++ tictactoe.js
@@ -3,14 +3,20 @@
     the board shape as well as a setBoard to set the Board
 */
 function Gameboard() {
-    let board = new Array(3);
+    let board = Array.from({length: 3}, () => []);
     for(let i = 0; i < board.length; i++) {
-        board[i] = new Array(3);
+        board[i] = Array.from({length: 3}, () => []);
     }
     const getBoard = () => board;
-    const setBoard = (x, y, value) => board[x,y] = value;
+
+    const getBoardPost = (x, y) => {
+        return board[x][y];
+    }
+    const setBoard = (x, y, value) => {
+        board[x][y] = value;
+    }
     
-    return {getBoard, setBoard};
+    return {getBoard, setBoard, getBoardPost};
 }
 
 /*
@@ -26,15 +32,17 @@ function Player(name, shape) {
     vertical, horizontal and diagonal areas.
     If there is such a mark and if matches 3 times, we consider that a winner
 */
+
 const checkSquare = (x,y,shape, board) => {
     let isWinner = false;
-    let count = 0;
+    let count = 1;
     //checks if count total 
     const checkCount = () => {
-        if(count === 3) {
-            isWinner = true;
+        console.log('This is a count', count)
+        if(count < 3) {
+            count = 1;
         } else {
-            count = 0;
+            isWinner = true;
         }
         return;
     }
@@ -44,8 +52,9 @@ const checkSquare = (x,y,shape, board) => {
         if(y-1 < 0) {
             return;
         }
-        while(y > 0) {
-            if(board[x, y] === shape) {
+        while(y-1 >= 0) {
+            if(board.getBoardPost(x,y-1) === shape) {
+                console.log(count);
                 count++;
                 y--;
             }
@@ -61,8 +70,8 @@ const checkSquare = (x,y,shape, board) => {
         if(y+1 > 2) {
             return;
         }
-        while(y < 3) {
-            if(board[x,y] === shape) {
+        while(y+1 < 3) {
+            if(board.getBoardPost(x,y+1) === shape) {
                 count++;
                 y++;
             }
@@ -78,9 +87,10 @@ const checkSquare = (x,y,shape, board) => {
         if(x-1 < 0) {
             return;
         }
-        while(x > 0) {
-            if(board[x,y] === shape) {
+        while(x-1 >= 0) {
+            if(board.getBoardPost(x-1,y) === shape) {
                 count++;
+                console.log(board.getBoard());
                 x--;
             }
              else {
@@ -96,8 +106,8 @@ const checkSquare = (x,y,shape, board) => {
         if(x+1 > 2) {
             return;
         }
-        while(x < 3) {
-            if(board[x,y] === shape) {
+        while(x+1 < 3) {
+            if(board.getBoardPost(x+1,y) === shape) {
                 count++;
                 x++;
             } else {
@@ -112,8 +122,8 @@ const checkSquare = (x,y,shape, board) => {
         if(x-1 < 0 && y-1 < 0) {
             return;
         }
-        while(x > 0 && y > 0) {
-            if(board[x,y] === shape) {
+        while(x-1 >= 0 && y-1 >= 0) {
+            if(board.getBoardPost(x-1,y-1) === shape) {
                 count++;
                 x--;
                 y--;
@@ -129,8 +139,8 @@ const checkSquare = (x,y,shape, board) => {
         if(x-1 < 0 && y+1 > 2) {
             return;
         }
-        while(x > 0 && y < 3) {
-            if(board[x,y] === shape) {
+        while(x-1 >= 0 && y+1 < 3) {
+            if(board.getBoardPost(x-1,y+1) === shape) {
                 count++;
                 x--;
                 y++;
@@ -146,8 +156,8 @@ const checkSquare = (x,y,shape, board) => {
         if(x+1 > 2 && y-1 < 0) {
             return;
         }
-        while(x < 3 && y > 0) {
-            if(board[x,y] === shape) {
+        while(x+1 < 3 && y-1 >= 0) {
+            if(board.getBoardPost(x+1,y-1) === shape) {
                 count++;
                 x++;
                 y--;
@@ -164,8 +174,8 @@ const checkSquare = (x,y,shape, board) => {
         if(x+1 > 2 && y+1 > 2) {
             return;
         }
-        while(x < 3 && y < 3) {
-            if(board[x,y] === shape) {
+        while(x+1 < 3 && y+1 < 3) {
+            if(board.getBoardPost(x+1,y+1) === shape) {
                 count++;
                 x++;
                 y++;
@@ -219,7 +229,7 @@ const checkSquare = (x,y,shape, board) => {
             return true;
         }
     }
-
+    count = 0;
     return false;
 }
 
@@ -230,22 +240,116 @@ const playGame = () => {
     //cannot go beyond 9
     //follow players input value on the board
     //logic to check if theres any value thats the same on all angles
+    let testboard = Gameboard();
     let player1 = Player('Sam', 'X');
     let player2 = Player('Dan', 'O');
     let round = 1;
-    let currentplayer = player1;
+    let currentplayer = true;
+    let winner;
     //creating a 9x9 board visually through DOMs manipulation
     //Player 1 whoever it is goes first and ask
+    let somebutton = document.createElement('button');
+    let container = document.querySelector('#container');
+    somebutton.setAttribute('id', 'abutton');
+    somebutton.textContent = 'Start';
+    somebutton.addEventListener('click', (e) => {
+        let tempx = Math.floor(Math.random() * 3);
+        let tempy = Math.floor(Math.random() * 3);
+        console.log("This is round", round)
+        if(currentplayer) {
+            //quick test on logic
+            console.log("This is X");
+            if(testboard.getBoardPost(tempx, tempy).length !== 0) {
+                console.log(testboard.getBoardPost(tempx, tempy))
+                console.log(tempx, tempy)
+                console.log('that space is taken');
+            } else {
+                console.log(tempx, tempy);
+                testboard.setBoard(tempx, tempy, player1.shape);
+                let result = checkSquare(tempx, tempy, player1.shape, testboard);
+                if(result) {
+                    winner = player1.name;
+                }
+                round++;
+                currentplayer = false;
+            }
+        } else {
+            console.log("This is O")
+            if(testboard.getBoardPost(tempx, tempy).length !== 0) {
+                console.log(tempx, tempy)
+                console.log('that space is taken');
+            } else {
+                console.log(tempx, tempy);
+                testboard.setBoard(tempx, tempy, player2.shape);
+                let result = checkSquare(tempx, tempy, player2.shape, testboard);
+                if(result) {
+                    winner = player2.name;
+                }
+                round++;
+                currentplayer = true;
+            }
+        }
+        
+        if(winner) {
+            console.log("The winner is", winner);
+            console.log(testboard.getBoard())
+        } else if(round === 9) {
+            console.log("Its a tie");
+        } else {
+            console.log('continue')
+            console.log(testboard.getBoard());
+        }
+        
+    });
+    container.appendChild(somebutton);
+    
+/*
+    while(round <= 9) {
+        let tempx = Math.floor(Math.random() * 2);
+        let tempy = Math.floor(Math.random() * 2);
+        let tempboard = testboard.getBoard()
+        if(currentplayer) {
+            //quick test on logic
+            if(tempboard[tempx][tempy] === 'X' || tempboard[tempx][tempy] === 'O') {
+                continue;
+            }
+            testboard.setBoard(tempx, tempy, player1.shape);
+            let result = checkSquare(tempx, tempy, player1.shape, tempboard);
+            if(result) {
+                winner = player1.name;
+                break;
+            }
+            round++;
+            currentplayer = false;
+        } else {
+            if(tempboard[tempx][tempy] === 'X' || tempboard[tempx][tempy] === 'O') {
+                continue;
+            }
+            testboard.setBoard(tempx, tempy, player1.shape);
+            let result = checkSquare(tempx, tempy, player2.shape, tempboard);
+            if(result) {
+                winner = player2.name;
+                break;
+            }
+            round++;
+            currentplayer = true;
+        }
+    }
+        */
 }
+/*
 
 let somestuff = Gameboard();
-somestuff.setBoard(1, 1, 'X');
-console.log(somestuff.getBoard());
+console.log('hit3')
+console.log(somestuff.getBoardPost(1, 0))
+somestuff.setBoard(0, 0, 'X');
+console.log(somestuff.getBoard())
 // let round = 1;
 let player1 = new Player('Sam', 'X');
-console.log(player1.piece);
+console.log(player1.shape) */
 let buttonexample = document.querySelector("#buttonthing")
-// buttonexample.addEventListener("click", () => {
-//     round++;
-//     console.log(round)
-// })
+
+buttonexample.addEventListener("click", () => {
+     playGame();
+ })
+ 
